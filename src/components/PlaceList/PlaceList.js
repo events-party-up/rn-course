1c13df2063e11bfde522c2cfc9e794c42f730b9c
Marcian Diamond <marciandmnd@yahoo.com>
2018-06-02 10:03:46 -0700

    Listening to touch events

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -4,14 +4,14 @@ import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
   const placesOutput = props.places.map((place, i) => (
-    <ListItem key={i} placeName={place} />
+    <ListItem key={i} placeName={place} onItemPressed={() => alert('pressed')}/>
   ));
 
   return (
     <View style={styles.listContainer}>
       {placesOutput}
     </View>
-    
+
   );
 }
 
