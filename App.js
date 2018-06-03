57b74b57dfd70765333c222def83283ade8fa758
Marcian Diamond <marciandmnd@yahoo.com>
2018-06-03 16:57:59 -0700

    Added SideDrawer

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawerScreen from './src/screens/SideDrawer/SideDrawer';
 
 
 import configureStore from './src/store/configureStore';
@@ -17,6 +18,7 @@ Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen, stor
 Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen, store, Provider);
 Navigation.registerComponent('awesome-places.PlaceDetailScreen', () => PlaceDetailScreen, store, Provider);
+Navigation.registerComponent('awesome-places.SideDrawerScreen', () => SideDrawerScreen);
 
 // Start an App for react-native-navigation
 Navigation.startSingleScreenApp({
