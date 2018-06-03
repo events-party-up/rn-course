fe0646845fb3bdd5871a1da4250cbdfdf5b02322
Marcian Diamond <marciandmnd@yahoo.com>
2018-06-03 13:47:35 -0700

    Added delete icon to place modal

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Modal, View, Image, Text, Button, StyleSheet } from 'react-native';
+import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const placeDetail = (props) => {
   let modalContent = null;
@@ -20,7 +21,11 @@ const placeDetail = (props) => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
-          <Button onPress={() => props.onItemDeleted()} title="Delete" color="red" />
+          <TouchableOpacity onPress={props.onItemDeleted}>
+            <View style={styles.deleteButton}>
+              <Icon size={30} name="ios-trash" color="red" />
+            </View>
+          </TouchableOpacity>
           <Button onPress={() => props.onModalClosed()} title="Close" />
         </View>
       </View>
@@ -42,6 +47,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     fontSize: 28
+  },
+  deleteButton: {
+    alignItems: 'center'
   }
 });
 
