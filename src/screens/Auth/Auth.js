da2f81db72f1c86ebd3f0299bf9271582d4eae1d
Marcian Diamond <marciandmnd@yahoo.com>
2018-06-04 18:17:50 -0700

    portrait/landscape styles for Auth component

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground, Dimensions } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
@@ -9,22 +9,59 @@ import backgroundImage from '../../assets/background.jpg';
 import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 
 class AuthScreen extends Component {
+  state = {
+    styles: {
+      pwContainerDirection: 'column',
+      pwContainerJustifyContent: 'flex-start',
+      pwWrapperWidth: '100%'
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener('change', dims => {
+      this.setState({
+        styles: {
+          pwContainerDirection: Dimensions.get('window').height > 500 ? 'column' : 'row',
+          pwContainerJustifyContent: Dimensions.get('window').height > 500 ? 'flex-start' : 'space-between',
+          pwWrapperWidth: Dimensions.get('window').height > 500 ? '100%' : '45%'
+        }
+      })
+    })
+  }
+
   loginHandler = () => {
     startMainTabs();
   }
   
   render() {
+    let headingText = null;
+
+    if (Dimensions.get('window').height > 500) {
+      headingText =  (
+        <MainText>
+          <HeadingText>Please Log In</HeadingText>
+        </MainText>
+      );
+    }
     return (
       <ImageBackground style={styles.backgroundImage} source={backgroundImage}>
         <View style={styles.container}>
-          <MainText>
-            <HeadingText>Please Log In</HeadingText>
-          </MainText>
+          {headingText}
           <ButtonWithBackground color="#29aaf4" onPress={() => alert("hello")}>Switch to Log In</ButtonWithBackground>
           <View style={styles.inputContainer}>
             <DefaultInput placeholder="Your Email Address" style={styles.input}/>
-            <DefaultInput placeholder="Password" style={[styles.input, {borderColor: 'red'}]}/>
-            <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+            <View style={{
+              flexDirection: this.state.styles.pwContainerDirection,
+              justifyContent: this.state.styles.pwContainerJustifyContent
+            }}>
+              <View style={{width: this.state.styles.pwWrapperWidth}}>
+                <DefaultInput placeholder="Password" style={[styles.input, {borderColor: 'red'}]} />
+              </View>
+              <View style={{width: this.state.styles.pwWrapperWidth}}>
+                <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+              </View>
+            </View>
           </View>
           <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>Submit</ButtonWithBackground>
         </View>
@@ -50,6 +87,13 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: '#eee',
     borderColor: '#bbb' 
+  },
+  passwordContainer: {
+    flexDirection: Dimensions.get('window').height > 500 ? 'column' : 'row',
+    justifyContent: 'space-between'
+  },
+  passwordWrapper: {
+    width: Dimensions.get('window').height > 500 ? "100%" : "45%"
   }
 });
 
