308aae70fa61c76b8e79158f9830a483822972d8
Marcian Diamond <marciandmnd@yahoo.com>
2018-06-05 13:51:05 -0700

    Validation error styles

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -18,21 +18,24 @@ class AuthScreen extends Component {
         valid: false,
         validationRules: {
           isEmail: true
-        }
+        },
+        touched: false
       },
       password: {
         value: '',
         valid: false,
         validationRules: {
           minLength: 6
-        }
+        },
+        touched: false
       },
       confirmPassword: {
         value: '',
         valid: false,
         validationRules: {
           equalTo: 'password'
-        }
+        },
+        touched: false
       }
     }
   }
@@ -93,7 +96,8 @@ class AuthScreen extends Component {
           [key]: {
             ...prevState.controls[key],
             value: value,
-            valid: validate(value, prevState.controls[key].validationRules, connectedValue)
+            valid: validate(value, prevState.controls[key].validationRules, connectedValue),
+            touched: true
           }
         }
       }
@@ -120,7 +124,9 @@ class AuthScreen extends Component {
               placeholder="Your Email Address"
               style={styles.input}
               value={this.state.controls.email.value}
-              onChangeText={(val) => this.updateInputState('email', val)} />
+              onChangeText={(val) => this.updateInputState('email', val)}
+              valid={this.state.controls.email.valid}
+              touched={this.state.controls.email.touched} />
             <View 
               style={
                 this.state.viewMode === 'portrait'
@@ -131,20 +137,33 @@ class AuthScreen extends Component {
               <View style={this.state.viewMode === 'portrait' ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
                 <DefaultInput
                   placeholder="Password"
-                  style={[styles.input, {borderColor: 'red'}]}
+                  style={styles.input}
                   value={this.state.controls.password.value}
-                  onChangeText={(val) => this.updateInputState('password', val)} />
+                  onChangeText={(val) => this.updateInputState('password', val)}
+                  valid={this.state.controls.password.valid}
+                  touched={this.state.controls.password.touched} />
               </View>
               <View style={this.state.viewMode === 'portrait' ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
                 <DefaultInput
                   placeholder="Confirm Password"
                   style={styles.input}
                   value={this.state.controls.confirmPassword.value}
-                  onChangeText={(val) => this.updateInputState('confirmPassword', val)} />
+                  onChangeText={(val) => this.updateInputState('confirmPassword', val)}
+                  valid={this.state.controls.confirmPassword.valid}
+                  touched={this.state.controls.confirmPassword.touched} />
               </View>
             </View>
           </View>
-          <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>Submit</ButtonWithBackground>
+          <ButtonWithBackground
+            color="#29aaf4"
+            onPress={this.loginHandler}
+            disabled={
+              !this.state.controls.confirmPassword.valid ||
+              !this.state.controls.email.valid ||
+              !this.state.controls.password.valid
+            }>
+            Submit
+          </ButtonWithBackground>
         </View>
       </ImageBackground>
       
