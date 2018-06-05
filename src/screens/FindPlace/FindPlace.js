b8657d6010802d930510cce47152b870efc02b41
Marcian Diamond <marciandmnd@yahoo.com>
2018-06-04 19:31:42 -0700

    Fade in places list

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -16,7 +16,8 @@ class FindPlaceScreen extends Component {
 
   state = {
     placesLoaded: false,
-    removeAnim: new Animated.Value(1)
+    removeAnim: new Animated.Value(1),
+    placesAnim: new Animated.Value(0)
   }
 
   constructor(props) {
@@ -47,12 +48,25 @@ class FindPlaceScreen extends Component {
     });
   }
 
+  placesLoadedHandler = () => {
+    Animated.timing(this.state.placesAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start()
+  }
+
   placesSearchHandler = () => {
     Animated.timing(this.state.removeAnim, {
       toValue: 0,
       duration: 500,
       useNativeDriver: true
-    }).start();
+    }).start(() => {
+      this.setState({
+        placesLoaded: true
+      });
+      this.placesLoadedHandler();
+    });
   }
 
   render () {
@@ -79,7 +93,13 @@ class FindPlaceScreen extends Component {
     )
     if (this.state.placesLoaded) {
       content = (
-        <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+        <Animated.View
+          style={{
+            opacity: this.state.placesAnim
+          }}
+        >
+          <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler}/>
+        </Animated.View>
       );
     }
     return (
